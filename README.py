e2db076bda77e05fcd5600bec77c079895ff3c4c
itgcrook182 <itgcrook182@students.sthelens.ac.uk>
2015-11-06 11:38:55 +0000

    Update README.py

--- README.py
+++ README.py
@@ -1,4 +1,6 @@
 # helloworld
+import turtle #I like turtles
 print("Hello World!")
 
-print(""" I am a college student learning Python. I intend to eventually learn C++ and C# in the future. """)
+print(""" I am a college student learning Python. I intend to eventually learn C++ and C# in the future.
+My ultimate goal is to go into computer programming""")
